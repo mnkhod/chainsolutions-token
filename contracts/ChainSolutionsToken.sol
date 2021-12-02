6600797e320a30760040371966c5ed2eb60d71ed
mnkhod <mnkhod.dev@gmail.com>
2021-12-02 20:26:57 +0800

    Added Votes Functionality To ChainSolutionsToken

--- contracts/ChainSolutionsToken.sol
+++ contracts/ChainSolutionsToken.sol
@@ -6,9 +6,10 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract ChainSolutionsToken is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable, ERC20Permit {
+contract ChainSolutionsToken is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable, ERC20Permit, ERC20Votes {
     constructor() ERC20("ChainSolutionsToken", "CST") ERC20Permit("ChainSolutionsToken") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
@@ -36,4 +37,28 @@ contract ChainSolutionsToken is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pa
     {
         super._beforeTokenTransfer(from, to, amount);
     }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+
 }
